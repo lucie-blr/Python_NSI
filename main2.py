13aee18cfff85827d50063176257b07c6d6d107e
LoliChann <l.otaku.roux@gmail.com>
2022-05-03 13:25:51 +0200

    add textured tiles

--- main2.py
+++ main2.py
@@ -7,7 +7,7 @@ pygame.init()
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 level = Level(level_map_2, screen)
-
+bg = pygame.image.load("./alien/background.jpg")
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -15,7 +15,8 @@ while True:
             sys.exit()
             
     screen.fill('black')
-    level.run()
+    screen.blit(bg,(0,0))
+    level.run() 
     
     pygame.display.update()
     clock.tick(60)
--- tile.py
+++ tile.py
@@ -13,8 +13,7 @@ class Tile(pygame.sprite.Sprite):
 class Tile_wall(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
-        self.image = pygame.Surface((size, size))
-        self.image.fill('red')
+        self.image = pygame.image.load("./alien/boxAlt.png")
         self.rect = self.image.get_rect(topleft = pos)
         
     def update(self, x_shift):
@@ -23,8 +22,7 @@ class Tile_wall(pygame.sprite.Sprite):
 class Tile_ground(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
-        self.image = pygame.Surface((size, size))
-        self.image.fill('green')
+        self.image = pygame.image.load("./alien/sss.png")
         self.rect = self.image.get_rect(topleft = pos)
         
     def update(self, x_shift):
