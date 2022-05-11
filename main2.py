36f85e77163f1d87806f1173f5bf6c30c297ad33
LoliChann <53198861+LoliChannFr@users.noreply.github.com>
2022-05-11 11:24:14 +0200

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,31 +1,22 @@
+from turtle import width
 import pygame
 from tile import *
 from settings import *
-from player import Player
+from player import Bullet, Player
 import time
 import sys
-import run
-import json
 
 class Level:
-    def __init__(self, level_data, surface, spawn):
+    def __init__(self, level_data, surface):
         self.display_surface = surface
-        self.spawn = spawn
-        self.setup(level_data, surface)
+        self.setup(level_data)
         self.world_shift = 0
         
         
-    def setup(self, layout_index, screen):
+    def setup(self, layout):
         self.tiles = pygame.sprite.Group()
         self.player = pygame.sprite.GroupSingle()
-        messages = levelsign[layout_index]
-        sign_index = 0
-        self.layout_index = layout_index
-        layout = levelmap[layout_index]
-        
-        
-        
-        
+        self.bullet = pygame.sprite.GroupSingle()
         for row_index,row in enumerate(layout):
             for col_index, cell in enumerate(row): 
                 if cell == 'T':
@@ -48,22 +39,16 @@ class Level:
                     y = row_index * tile_size
                     tile = Tile_wall((x,y),tile_size)
                     self.tiles.add(tile)
-                if cell == 'L':
-                    x = col_index * tile_size
-                    y = row_index * tile_size
-                    tile = Tile_sign((x,y),tile_size)
-                    tile.sign[1] = messages[sign_index]
-                    self.tiles.add(tile)
-                    print(sign_index)
-                    sign_index = sign_index + 1
                 if cell == 'P':
                     x = col_index * tile_size
                     y = row_index * tile_size
-                    
                     player_sprite = Player((x,y))
-                        
                     self.player.add(player_sprite)
-                    print(self.spawn)
+                if cell == 'B':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+                    player_sprite = Bullet((x,y))
+                    self.bullet.add(player_sprite)
                 if cell == 'S':
                     x = col_index * tile_size
                     y = row_index * tile_size
@@ -74,11 +59,6 @@ class Level:
                     y = row_index * tile_size
                     tile = Tile_s((x,y),tile_size)
                     self.tiles.add(tile)
-                if cell == 'F':
-                    x = col_index * tile_size
-                    y = row_index * tile_size
-                    tile = Tile_end((x,y),tile_size)
-                    self.tiles.add(tile)
         
     def scroll_x(self):
         player = self.player.sprite
@@ -86,11 +66,11 @@ class Level:
         direction_x = player.direction.x
         width, h = pygame.display.get_surface().get_size()
         
-        if player_x < width / 3 and direction_x < 0:
+        if player_x < width / 5 and direction_x < 0:
             self.world_shift = 5
             player.speed = 0
         
-        elif player_x > width - (width / 2) and direction_x > 0:
+        elif player_x > width - (width / 5) and direction_x > 0:
             self.world_shift = -5
             player.speed = 0
         
@@ -98,174 +78,92 @@ class Level:
             self.world_shift = 0
             player.speed = 5
         
-    def horizontal_mouvement_collision(self,screen, level_map):
+    def horizontal_mouvement_collision(self):
         player = self.player.sprite
         
         player.rect.x += player.direction.x * player.speed
         
         for sprite in self.tiles.sprites():
             if sprite.rect.colliderect(player.rect):
-                try:
-                    if sprite.end:
-                        with open("data.json", "r") as f:	#config size screen
-                            data = json.load(f)
-                            WIDTH = data["WIDTH"]
-                            HEIGHT = data["HEIGHT"]
-                        text_font = pygame.font.Font(None, 60)  #Text Font
-                        white = (255,255,255)
-                        text = text_font.render("Level finished !", True, white)
-                        textRect = text.get_rect()
-                        textRect.center = (WIDTH/2, HEIGHT/2)
-                        screen.blit(text, textRect)
-                        player.speed = 0
-                        player.status = "idle"
-                        self.world_shift = 0
-                        if player.death == 4:
-                            level_map = level_map + 1
-                            data["unlock"][0][f"map{level_map}"] = "True"
-                            with open (f"data.json", "w") as f:
-                                json.dump(data,f)
-                            run.main()
-                        
-                except AttributeError: 
-                    if sprite.sign[0]:
-                        
-                        text_font = pygame.font.Font(None, 40)  #Text Font
-                        white = (255,255,255)
-                        text = text_font.render(sprite.sign[1], True, white)
-                        textRect = text.get_rect()
-                        textRect.center = (sprite.rect.x, sprite.rect.y - 48)
-                        screen.blit(text, textRect)
-                        
-                    else:    
-                        
-                        if sprite.damage == True:
-                            
-                            #Death animation
-                            player.status = 'death'
-                            if player.death == 20:
-                                
-                                main(self.layout_index)  
-                        
-                        else:    
+                if sprite.damage == True:
+                    
+                    #Death animation
+                    player.status = 'death'
+                    if player.death == 20:
+                        main()
 
-                            if player.direction.x < 0:
-                                player.rect.left = sprite.rect.right
-                                if sprite.climb:
-                                    player.direction.y = 1
-                                    player.double_jump = 1
-                            elif player.direction.x > 0:
-                                player.rect.right = sprite.rect.left
-                                if sprite.climb:
-                                    player.direction.y = 1
-                                    player.double_jump = 1
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                    if sprite.climb:
+                        player.direction.y = -0.8
+                        player.double_jump = 1
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
+                    if sprite.climb:
+                        player.direction.y = -0.8
+                        player.double_jump = 1
             else: player.gravity = 0.8
                     
                     
         
-    def vertical_mouvement_collision(self,screen, level_map):
+    def vertical_mouvement_collision(self):
         player = self.player.sprite
         player.apply_gravity()
         
         for sprite in self.tiles.sprites():
             if sprite.rect.colliderect(player.rect):
-                try:
-                    if sprite.end:
-                        with open("data.json", "r") as f:	#config size screen
-                            data = json.load(f)
-                            WIDTH = data["WIDTH"]
-                            HEIGHT = data["HEIGHT"]
-                        text_font = pygame.font.Font(None, 60)  #Text Font
-                        white = (255,255,255)
-                        text = text_font.render("Level finished !", True, white)
-                        textRect = text.get_rect()
-                        textRect.center = (WIDTH/2, HEIGHT/2)
-                        screen.blit(text, textRect)
-                        player.speed = 0
-                        player.status = "win"
-                        self.world_shift = 0
-                        if player.death == 4:
-                            run.main()
-                        
-                except AttributeError: 
-                    if sprite.sign[0]:
-                        
-                        text_font = pygame.font.Font(None, 40)  #Text Font
-                        white = (255,255,255)
-                        dark = (0,0,0)
-                        text = text_font.render(sprite.sign[1], True, white, dark)
-                        textRect = text.get_rect()
-                        
-                        textRect.center = (sprite.rect.x, sprite.rect.y - 48)
-                        screen.blit(text, textRect)
-                    else:    
-                        
-                        if sprite.damage == True:
-                        #Death animation
-                            player.status = 'death'
-                        if player.death == 20:
-                            main(self.layout_index)  
-                            
-                        else:
-                            if player.direction.y > 0:
-                                player.rect.bottom = sprite.rect.top
-                                player.direction.y = 0
-                                player.double_jump = 1
-                                player.time = time.time()
-                            elif player.direction.y < 0:
-                                player.rect.top = sprite.rect.bottom
-                                player.direction.y = 0
+                if sprite.damage == True:
+                   #Death animation
+                    player.status = 'death'
+                    if player.death == 20:
+                        main()  
+
                     
-    
-    def run(self,screen, level_map):
+                if player.direction.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                    player.direction.y = 0
+                    player.double_jump = 1
+                    player.time = time.time()
+                elif player.direction.y < 0:
+                    player.rect.top = sprite.rect.bottom
+                    player.direction.y = 0
+
+    def bullet_update(self):
+        player = self.player.sprite
+        bullet = self.bullet.sprite 
+        if player.bullet and bullet.Ask_bullet:
+            bullet.bullet(player)               
+
+
+    def run(self):
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        self.bullet_update()
+        self.bullet.update()
+        self.bullet.draw(self.display_surface)
         self.scroll_x()
         
         self.player.update()
-        self.horizontal_mouvement_collision(screen, level_map)
-        self.vertical_mouvement_collision(screen, level_map)
+        self.horizontal_mouvement_collision()
+        self.vertical_mouvement_collision()
         self.player.draw(self.display_surface)
-        
-        with open("data.json", "r") as f:	#config size screen
-            data = json.load(f)
-            WIDTH = data["WIDTH"]
-            HEIGHT = data["HEIGHT"]
-        
-        # Text
-        text_font = pygame.font.Font(None, 22)	#Text Font
-        white = (255,255,255)
-        text = text_font.render('v 1.0', True, white)
-        # create a rectangular object for the text
-        textRect = text.get_rect()
-        textRect.center = (WIDTH-40 , HEIGHT-20)
-        screen.blit(text, textRect)
-        
-        # Text
-        text_font = pygame.font.Font(None, 40)	#Text Font
-        white = (255,255,255)
-        text = text_font.render(f'Level {level_map}', True, white)
-        # create a rectangular object for the text
-        textRect = text.get_rect()
-        textRect.center = (WIDTH - (WIDTH-80) , HEIGHT - (HEIGHT-20))
-        screen.blit(text, textRect)
 
-def main(level_map):
+        
+
+def main():
     pygame.init()
-    with open("data.json", "r") as f:	#config size screen
-        data = json.load(f)
-        WIDTH = data["WIDTH"]
-        HEIGHT = data["HEIGHT"]
-        FULL = data["FULL"]
-
-    if FULL == "None":	
-        screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    else:
+    '''
+    width = input("Width (\"full\" to get fullscreen):")
+    if width == "full":
         screen = pygame.display.set_mode()
-        WIDTH, HEIGHT = screen.get_size()
+    else:
+        width = int(width)
+        height = int(input("Height :"))
+        screen = pygame.display.set_mode((width, height))
+    '''
+    screen = pygame.display.set_mode((1200, 700))
     clock = pygame.time.Clock()
-    spawn = "null"
-    level = Level(level_map, screen, spawn)
+    level = Level(level_map_2, screen)
     width, height = screen.get_size()
     bg = pygame.image.load("./alien/background.jpg")
     
@@ -277,11 +175,10 @@ def main(level_map):
                 
         screen.fill('black')
         screen.blit(bg,(0,0))
-        level.run(screen, level_map) 
+        level.run() 
         
         pygame.display.update()
         clock.tick(60)
         
 if __name__ == "__main__":
-    spawn = "null"
-    main(spawn)
+    main()
--- tile.py
+++ tile.py
@@ -1,18 +1,16 @@
 import pygame
 
-#Création des différents type de block avec des class
-
-class Tile(pygame.sprite.Sprite): #Création de la class
-    def __init__(self, pos, size): #Fonction qui se lance au lancement de la classe
-        super().__init__() 
-        self.image = pygame.image.load("./alien/tile.png") #Définition de l'image
-        self.rect = self.image.get_rect(topleft = pos) #Récupération de la position du coin en bas à gauche de l'image 
-        self.damage = False #Définition de si l'objet peut faire des dégats au joueur
-        self.climb = False #Définition de si l'objet peut être grimpé par le joueur
-        self.sign = [False, "bonjour"] #Définition de si l'objet est un panneau et du message de l'objet
-         
-    def update(self, x_shift): #Fonction qui change les variables de l'objet
-        self.rect.x += x_shift #Ajoute à la position en x de l'objet la vitesse de déplacement de la map
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, size):
+        super().__init__()
+        self.image = pygame.Surface((size, size))
+        self.image.fill('grey')
+        self.rect = self.image.get_rect(topleft = pos)
+        self.damage = False
+        self.climb = False
+        
+    def update(self, x_shift):
+        self.rect.x += x_shift
 
 class Tile_wall(pygame.sprite.Sprite):
     def __init__(self, pos, size):
@@ -21,7 +19,6 @@ class Tile_wall(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
         self.damage = False
         self.climb = True
-        self.sign = [False, "bonjour"]
     def update(self, x_shift):
         self.rect.x += x_shift
         
@@ -32,7 +29,6 @@ class Tile_ground(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
         self.damage = False
         self.climb = False
-        self.sign = [False, "bonjour"]
     def update(self, x_shift):
         self.rect.x += x_shift
 
@@ -43,7 +39,6 @@ class Tile_spike(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(bottomleft = pos)
         self.damage = True
         self.climb = False
-        self.sign = [False, "bonjour"]
     def update(self, x_shift):
         self.rect.x += x_shift
 
@@ -54,7 +49,6 @@ class Tile_s(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
         self.damage = False
         self.climb = False
-        self.sign = [False, "bonjour"]
     def update(self, x_shift):
         self.rect.x += x_shift
 
@@ -65,45 +59,5 @@ class Tile_earth(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
         self.damage = False
         self.climb = True
-        
-        self.sign = [False, "bonjour"]
-    def update(self, x_shift):
-        self.rect.x += x_shift
-
-class Tile_checkpoint(pygame.sprite.Sprite):
-    def __init__(self, pos, size):
-        super().__init__()
-        self.image = pygame.image.load("./alien/Sans titre.png")
-        self.rect = self.image.get_rect(topleft = pos)
-        self.damage = False
-        self.climb = False
-        
-        self.sign = [False, "bonjour"]
- 
-    def update(self, x_shift):
-        self.rect.x += x_shift
-
-class Tile_sign(pygame.sprite.Sprite):
-    def __init__(self, pos, size):
-        super().__init__()
-        self.image = pygame.image.load("./alien/sign.png")
-        self.rect = self.image.get_rect(topleft = pos)
-        self.damage = False
-        self.climb = False
-        self.sign = [True, "Use left and right keys to move"]
- 
-    def update(self, x_shift):
-        self.rect.x += x_shift
-        
-class Tile_end(pygame.sprite.Sprite):
-    def __init__(self, pos, size):
-        super().__init__()
-        self.image = pygame.image.load("./alien/flag.png")
-        self.rect = self.image.get_rect(topleft = pos)
-        self.damage = False
-        self.climb = False
-        self.sign = [False, "Use left and right keys to move"]
-        self.end = True
- 
     def update(self, x_shift):
         self.rect.x += x_shift
